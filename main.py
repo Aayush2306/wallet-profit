21c88b11a6b3d7add2725cc6888a302e8f9fd039
Aayush <64463251+Aayush2306@users.noreply.github.com>
2023-05-06 18:30:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -145,71 +145,72 @@ def profitWalletValue(wallet):
 
 @bot.message_handler(commands=['profitable'])
 def profitable(message):
-  #try:
-  a = []
-  ca = message.text.split(" ")[1]
-  ca = w3.toChecksumAddress(ca)
-  url = f'https://api.etherscan.io/api?module=account&action=txlist&address={ca}&startblock=0&endblock=99999999&sort=asc&apikey={ethApi}'
-  response = requests.get(url)
-  response_json = response.json()
-  transaction_hash = response_json['result'][0]['hash']
+  try:
+    a = []
+    ca = message.text.split(" ")[1]
+    ca = w3.toChecksumAddress(ca)
+    url = f'https://api.etherscan.io/api?module=account&action=txlist&address={ca}&startblock=0&endblock=99999999&sort=asc&apikey={ethApi}'
+    response = requests.get(url)
+    response_json = response.json()
+    transaction_hash = response_json['result'][0]['hash']
 
-  url = f'https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash={transaction_hash}&apikey={ethApi}'
-  response = requests.get(url)
-  response_json = response.json()
-  block_number_hex = response_json['result']['blockNumber']
-  block_number = int(block_number_hex, 16)
-  latest_block_number = int(w3.eth.blockNumber)
-  next_block = block_number + 10000
-  if latest_block_number < next_block:
-    next_block = latest_block_number
+    url = f'https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash={transaction_hash}&apikey={ethApi}'
+    response = requests.get(url)
+    response_json = response.json()
+    block_number_hex = response_json['result']['blockNumber']
+    block_number = int(block_number_hex, 16)
+    latest_block_number = int(w3.eth.blockNumber)
+    next_block = block_number + 10000
+    if latest_block_number < next_block:
+      next_block = latest_block_number
 
-  topic_hash = Web3.keccak(text='Transfer(address,address,uint256)').hex()
-  event_filter = w3.eth.filter({
+    topic_hash = Web3.keccak(text='Transfer(address,address,uint256)').hex()
+    event_filter = w3.eth.filter({
     'fromBlock': block_number,
     'toBlock': next_block,
     'address': ca,
     '  topics': [topic_hash]
   })
-  transfer_events = event_filter.get_all_entries()[3:100]
-  for event in transfer_events:
-    if len(event['topics']) < 3:
-      continue
+    transfer_events = event_filter.get_all_entries()[3:100]
+    for event in transfer_events:
+      if len(event['topics']) < 3:
+        continue
 #print(event['topics'])
-    toAds = "0x" + (event['topics'][2].hex())[26:]
+      toAds = "0x" + (event['topics'][2].hex())[26:]
 
-    if toAds.lower() != ca.lower():
+      if toAds.lower() != ca.lower():
       #print(toAds)
-      if toAds not in a:
-        a.append(toAds)
+        if toAds not in a:
+          a.append(toAds)
 
 #print(a)
-  b = a[:30]
-  my_array = []
-  for w in b:
-    profitkya = profitWalletValue(w)
-    my_array.append(profitkya)
+    b = a[:30]
+    my_array = []
+    for w in b:
+      profitkya = profitWalletValue(w)
+      my_array.append(profitkya)
   #print(profitarray)
-  my_array = [
-    obj for obj in my_array if all(v is not None for v in obj.values())
+    my_array = [
+      obj for obj in my_array if all(v is not None for v in 
+  obj.values())
   ]
-  sorted_balances = sorted(my_array, key=lambda x: x["profit"], reverse=True)
-  top_5_balances = sorted_balances[:10]
-  print(top_5_balances)
-  text = ""
-  for value in top_5_balances:
-    wallet = value["wallet"]
-    profit = value["profit"]
-    link = f"<a href='etherscan.io/address/{wallet}'>Wallet</a>"
-    text = text + f"{link} : <pre>{wallet}</pre>\nProfit: {profit} ETH\n\n "
+    sorted_balances = sorted(my_array, key=lambda x: x["profit"], reverse=True)
+    top_5_balances = sorted_balances[:10]
+    #print(top_5_balances)
+    text = ""
+    for value in top_5_balances:
+      wallet = value["wallet"]
+      profit = value["profit"]
+      link = f"<a href='etherscan.io/address/{wallet}'>Wallet</a>"
+      text = text + f"{link} : <pre>{wallet}</pre>\nProfit: {profit} ETH\n\n "
 
-  bot.send_message(message.chat.id,
+    bot.send_message(message.chat.id,
                    f"<b>Most profitable wallets from the token</b>\n\n{text}",
                    parse_mode="html",
                    disable_web_page_preview=True)
 
 
-#except:
-# print("no")
+  except:
+    print("no")
 
 bot.polling()
